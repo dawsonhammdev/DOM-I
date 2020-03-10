03e8678017702cd418c0d6e6ff71aa09c7cd4803
dawsonhammdev <hammsdesigns@gmail.com>
2020-03-10 10:38:46 -0500

    I have made the site look like the original design

--- js/index.js
+++ js/index.js
@@ -47,4 +47,50 @@ idSource.setAttribute('src', 'img/header-img.png');
 const idSourceMid = document.getElementById('middle-img');
 idSourceMid.setAttribute('src', 'img/mid-page-accent.jpg');
 
+let cta1 = document.querySelector('button');
+cta1.textContent = siteContent["cta"]["button"];
+
+let cta = document.querySelector('h1');
+cta.textContent = siteContent["cta"]["h1"];
+
+let mainContentTop = document.querySelectorAll('a');
+mainContentTop[0].textContent = siteContent["nav"]["nav-item-1"];
+mainContentTop[1].textContent = siteContent["nav"]["nav-item-2"];
+mainContentTop[2].textContent = siteContent["nav"]["nav-item-3"];
+mainContentTop[3].textContent = siteContent["nav"]["nav-item-4"];
+mainContentTop[4].textContent = siteContent["nav"]["nav-item-5"];
+mainContentTop[4].textContent = siteContent["nav"]["nav-item-6"];
+
+
+let mainContent = document.querySelectorAll('h4');
+mainContent[0].textContent = siteContent["main-content"]["features-h4"];
+mainContent[1].textContent = siteContent["main-content"]["about-h4"];
+mainContent[2].textContent = siteContent["main-content"]["services-h4"];
+mainContent[3].textContent = siteContent["main-content"]["product-h4"];
+mainContent[4].textContent = siteContent["main-content"]["vision-h4"];
+
+let mainContent1 = document.querySelectorAll('p');
+mainContent1[0].textContent = siteContent["main-content"]["features-content"];
+mainContent1[1].textContent = siteContent["main-content"]["about-content"];
+mainContent1[2].textContent = siteContent["main-content"]["services-content"];
+mainContent1[3].textContent = siteContent["main-content"]["product-content"];
+mainContent1[4].textContent = siteContent["main-content"]["vision-content"];
+
+let contact1 = document.querySelector('.contact h4');
+contact1.textContent = siteContent["contact"]["contact-h4"];
+
+let contact2 = document.querySelectorAll('.contact p');
+contact2[0].textContent = siteContent["contact"]["address"];
+contact2[1].textContent = siteContent["contact"]["phone"];
+contact2[2].textContent = siteContent["contact"]["email"];
+
+
+
+
+let footer1 = document.querySelector('footer');
+footer1.textContent = siteContent["footer"]["copyright"];
+
+
+
+
 
